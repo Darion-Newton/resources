876524720a871a0621a6b8fcc96bae7f6079c4bc
Darion <pixelarraymedia@gmail.com>
2021-12-09 05:19:01 -0800

    adding badge

--- client/src/components/layouts/Landing.js
+++ client/src/components/layouts/Landing.js
@@ -29,7 +29,13 @@ class Landing extends Component {
                    <h2 className="text-xl lg:text-xl font-medium mb-1 text-gray-700 text-center">Click here for your excellent free file</h2>
               <span className="text-sm mb-4 text-gray-400">Pop me a message and let's connect.</span>
                     </div>
-      
+                    <div class="bg-indigo-900 text-center py-4 lg:px-4">
+  <div class="p-2 bg-indigo-800 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex" role="alert">
+    <span class="flex rounded-full bg-indigo-500 uppercase px-2 py-1 text-xs font-bold mr-3">File Name</span>
+    <span class="font-semibold mr-2 text-left flex-auto">Click here for your excellent free file </span>
+    <svg class="fill-current opacity-75 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z"/></svg>
+  </div>
+</div>
                 <div id="nav_content" className={this.state.toggle ? 'absolute right-0  md:block md:relative w-full md:w-auto ':
                    'absolute hidden right-0 md:block md:relative w-full md:w-auto ' }>
                   <nav
