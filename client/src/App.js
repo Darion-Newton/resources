0dd9a76ef8c8595a6d23377d7248f72a94b4ca73
Darion <pixelarraymedia@gmail.com>
2021-12-08 17:21:43 -0800

    design partly done  still need to figure out  how to stay submitonclick onsuccess

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,31 @@
 import "tailwindcss/tailwind.css";
 import './App.css';
+import Landing from "./components/layouts/Landing";
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 
-function App() {
-  return (
-    <div  className="App">
-        <Landing />
-    </div>
-  );
-}
+import './App.css';
+
+
+import './styles/index.css';
+
+const App = () => (
+  
+  <Router>
+  <Fragment>
+ 
+  
+
+    <Route exact path="/" component={Landing} />
+
+    <section className="container">
+
+    </section>
+
+    
+  </Fragment>
+</Router>
+
+);
 
 export default App;
--- client/src/components/layouts/Landing.js
+++ client/src/components/layouts/Landing.js
@@ -1,50 +1,86 @@
-import React from "react";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
+class Landing extends Component {
 
-const Resource = () => {
+  constructor(props) {
+    super(props)
+  
+      this.state = {
+        toggle: true };
+      this.clickHandler = this.clickHandler.bind(this)
+  
+  }
+
+  clickHandler(){
+    this.setState(prevState => ({
+      
+      toggle: !prevState.toggle
+  }));
+  
+  console.log(this)
+  }
+  
+  render() {
     return (
-        <div>
-     
-
-     <div className="flex items-center ml-3">
-            <a href="/">
- <h1 className="logo">pixel.<span className="font-semibold ">ARRAY</span></h1>
- <p className="text-xs text-bold font-semibold uppercase text-center text-gray-900">Web - Digital Marketing - SEO</p>
-            </a>
-          </div>
-          <div className="inline-block h-8 mr-3 lg:hidden">
-            <button id="nav_toggle" class="h-full  w-full" onClick={this.clickHandler}>
-            <div></div>
-            <svg class="h-full w-auto" xmlns="http://www.w3.org/2000/svg" width="192" height="192" fill="#4B5563"
-              viewBox="0 0 256 256">
-              <rect width="256" height="256" fill="none"></rect>
-              <line x1="40" y1="128" x2="216" y2="128" stroke="#4B5563" stroke-linecap="round" stroke-linejoin="round"
-                stroke-width="16"></line>
-              <line x1="40" y1="64" x2="216" y2="64" stroke="#4B5563" stroke-linecap="round" stroke-linejoin="round"
-                stroke-width="16"></line>
-              <line x1="40" y1="192" x2="216" y2="192" stroke="#4B5563" stroke-linecap="round" stroke-linejoin="round"
-                stroke-width="16"></line>
-            </svg>
-          </button>
+      <div className="lg:flex">
+          <div className="flex justify-center" className={this.state.toggle ? ' hidden':
+                   'uppercase px-8 py-4 shadow-sm rounded inline-block mt-8 font-semibold hover:bg-blue-500 bg-blue-600 text-white ' }>
+                   <h2 className="text-xl lg:text-xl font-medium mb-1 text-gray-700 text-center">Click here for your excellent free file</h2>
+              <span className="text-sm mb-4 text-gray-400">Pop me a message and let's connect.</span>
+                    </div>
+      
+                <div id="nav_content" className={this.state.toggle ? 'absolute right-0  md:block md:relative w-full md:w-auto ':
+                   'absolute hidden right-0 md:block md:relative w-full md:w-auto ' }>
+                  <nav
+                    className="w-full bg-white md:bg-transparent rounded shadow-lg px-6 py-4 mt-4 text-center md:p-0 md:mt-0 md:shadow-none">
+                    <ul className="md:flex items-center">
+                    <div className="flex-grow justify-center bg-white px-8 lg:px-20 py-20 lg:py-48">
+          <div className="flex justify-center">
+            <div>
+              <h2 className="text-xl lg:text-xl font-medium mb-1 text-gray-700">Super fantastic call to action</h2>
+              <span className="text-sm mb-4 text-gray-400">Connect today!.</span>
+          
+              <form name="contact" data-netlify="true" netlify-honeypot="bot-field" method="POST" autocomplete="off" >
+                <p className="hidden">
+                  <label>Don’t fill this out if you’re human: <input name="bot-field" /></label>
+                </p>
+                <input type="hidden" name="form-name" value="contact"/>
+                <div className="input-animated">
+                  <input type="text" id="name" name="name" required pattern="\S+.*" placeholder="the placeholder" />
+                  <label htmlFor="name" className="label-name"><span className="content-name">Name</span></label>
+                </div>
+                <div className="input-animated">
+                  <input type="email" id="email" name="email" required placeholder="the placeholder" />
+                  <label htmlFor="email" className="label-email"><span className="content-email">Email</span></label>
+                </div>
+                <div>
+                </div>
+                <div data-netlify-recaptcha="true"></div>
+                <button onClick={this.clickHandler} type="submit" id="submit-button"
+                  className="w-full font-semibold rounded-tl-3xl rounded-br-3xl uppercase text-center px-5 py-3 shadow-md my-4 lg:mb-0 bg-gray-900 text-yellow-50 hover:bg-gray-700 focus:ring-4 focus:ring-yellow-300">Send
+                  Message</button>
+                <span id="success"
+                  className="hidden absolute mt-2 px-4 py-2 font-medium border rounded-sm border-green-500 bg-green-100 text-green-600">Thanks!
+                  We'll be in touch soon.</span>
+                <span id="error"
+                  className="hidden absolute mt-2 px-4 py-2 font-medium border rounded-sm border-red-500 bg-red-100 text-red-600">Whoops...
+                  Something went wrong.</span>
+      
+              </form>
             </div>
-          <div id="nav_content"
-            className={this.state.toggle ? ' w-full hidden py-8 lg:flex lg:w-auto lg:py-0 lg:items-center rounded-br-full shadow-md lg:shadow-none bg-blue-5' :
-             ' w-full  py-8 lg:flex lg:w-auto lg:py-0 lg:items-center rounded-br-full shadow-md lg:shadow-none bg-blue-5' }>
-            <ul className="flex flex-col ml-auto text-md text-gray-700 font-semibold lg:flex-row">
-              <li className="mx-6 w-mc hover:text-white mb-4 lg:mb-0 "><Link to="/report"> Free Website Report</Link></li>
-              <li className="mx-6 w-mc hover:text-white mb-4 lg:mb-0 "><Link to="/contact"> Contact</Link> </li>
-            </ul>
           </div>
-
-
-
-
-          </div>
-    
-
-
-
+        </div>
+                    </ul>
+                  </nav>
+                  </div>
+        
+      </div>
+      
+      
+  
     )
+  }
 }
 
-export default Resource
+export default Landing
