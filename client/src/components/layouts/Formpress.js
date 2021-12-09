728bb6a0fdd2d450dd14d47cb35002177d2a9a49
Darion <pixelarraymedia@gmail.com>
2021-12-09 09:29:32 -0800

    edited css for all forms

--- client/src/components/layouts/Formpress.js
+++ client/src/components/layouts/Formpress.js
@@ -31,8 +31,11 @@ class Formpress extends Component {
                    'uppercase px-8 py-4 shadow-sm rounded inline-block mt-8 font-semibold text-white ' }>
 <div class="bg-blue-100 border-t-4 border-blue-500 rounded-b text-blue-900 px-4 py-3 shadow-md" role="alert">
   <div class="flex">
+
+  
     <div class="py-1"><svg class="fill-current h-6 w-6 text-blue-500 mr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"/></svg></div>
-      <span className="font-bold ">File Name</span>
+    <span className="font-bold ">File Name</span>
+     
       <div className="py-8">
       <span className="text-sm inline-block">  <button className="flex-shrink-0 bg-blue-500 hover:bg-blue-700 border-blue-500 hover:border-blue-700 text-sm border-4 text-white py-1 px-2 rounded" type="button"  onClick={this.clickHandler}>
    Download
