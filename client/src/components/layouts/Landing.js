c3cdcb96769f39b1e66b4e9149bfefe1d74bd3b5
Darion <pixelarraymedia@gmail.com>
2021-12-09 09:17:49 -0800

    edited css for all forms

--- client/src/components/layouts/Landing.js
+++ client/src/components/layouts/Landing.js
@@ -27,14 +27,13 @@ class Landing extends Component {
           <div className={this.state.toggle ? 'hidden':
                    'uppercase px-8 py-4 shadow-sm rounded inline-block mt-8 font-semibold text-white bg-blue-900 ' }>
                     <div className="bg-blue-900 text-center py-4 lg:px-4">
-  <div className="p-2 bg-blue-800 items-center text-blue-100 leading-none rounded-full flex lg:inline-flex" role="alert">
+  <div className="p-2 bg-blue-800 items-center text-blue-100 leading-none lg:rounded-full flex lg:inline-flex" role="alert">
     <span className="flex rounded-full bg-blue-500 uppercase px-2 py-1 text-xs font-bold mr-3"> Thank you!</span>
-    <span class="font-semibold mr-2 text-left flex-auto text-center">Get your free resource now, click 'download' <button onClick={this.clickHandler}
-                  className=" w-full font-semibold rounded-tl-3xl rounded-br-3xl uppercase text-center px-5 py-3 shadow-md my-4 lg:mb-0 bg-gray-900 text-yellow-50 hover:bg-gray-700 focus:ring-4 focus:ring-yellow-300">Download</button> </span>
-                      <span class="font-semibold mr-2 text-left flex-auto">  <button onClick={this.clickHandler}
-                  className="flex rounded-full bg-blue-500 uppercase px-2 py-1 text-xs font-bold mr-3">Preview</button> </span>
-                        <span class="font-semibold mr-2 text-left flex-auto">  <button onClick={this.clickHandler}
-                  className="flex rounded-full bg-blue-500 uppercase px-2 py-1 text-xs font-bold mr-3">Go back  <svg class="fill-current opacity-75 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z"/></svg></button> </span>
+    <span class="font-semibold mr-2 text-left flex-auto text-center"> Click to 'download' your free template <button onClick={this.clickHandler}
+                  className=" font-semibold rounded-tl-3xl rounded-br-3xl uppercase text-center px-5 py-3 shadow-md my-4 lg:mb-0 bg-gray-900 text-yellow-50 hover:bg-gray-700 focus:ring-4 focus:ring-yellow-300">Download</button> </span>
+           <button onClick={this.clickHandler}
+                 > <svg class="fill-current opacity-75 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z"/></svg></button>
+                     
 
   </div>
 </div>
